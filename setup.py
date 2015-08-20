8004eb4cf4f35d61e487081540a16d096b6df899
chaitanya <chaitanya@micropyramid.com>
2015-08-20 18:59:29 +0530

    django simple pagination

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django_simple_pagination',
+    version='0.1',
+    packages=['simple_pagination'],
+    include_package_data=True,
+    description='A simple Django app to conduct Web-based polls.',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Chaitanya',
+    author_email='chaitanya@micropyramid.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        "django",
+    ],
+)
